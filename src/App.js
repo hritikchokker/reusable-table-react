a283230392fd7b5f81ef0a040ce53d1885aa4637
hritikchokker <hchokker7@gmail.com>
2021-04-02 12:09:04 +0530

    3 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
-
+import BasicColumnsGrid from "./table/Tabletest.js";
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <BasicColumnsGrid />
     </div>
   );
 }
