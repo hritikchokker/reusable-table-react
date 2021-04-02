a5ea2f2f8ff32df7150672442cf1e79927dabc82
hritikchokker <hchokker7@gmail.com>
2021-04-02 12:52:12 +0530

    1 file modified

--- src/user-module/userlist.js
+++ src/user-module/userlist.js
@@ -1,25 +1,26 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import BasicColumnsGrid from ".././table/Tabletest";
 export default function UserList() {
   const [userRows, setUserRows] = useState([]);
   const [userColums, setUserColumns] = useState([]);
-  // useLayoutEffect(() => {
-  //   // fetch("https://jsonplaceholder.typicode.com/users")
-  //   //   .then(res => res.json())
-  //   //   .then(res => {
-  //   //     console.log(res, "res");
-  //   //     if(res){
-  //   //       const item = res[0];
-  //   //       const clm=[];
-  //   //       Object.keys(item=>{
-  //   //         clm.push({field:item});
-  //   //       })
-  //   //       setUserColumns(clm)
-  //   //       setUserRows(res);
-  //   //     }
-  //   //   })
-  //   //   .catch(err => console.log(err));
-  // }, []);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(res => {
+        console.log(res, "res");
+        if (res) {
+          const item = res[0];
+          const clm = [];
+          Object.keys(item => {
+            clm.push({ field: item });
+          });
+          setUserColumns(clm);
+          setUserRows(res);
+        }
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <>
       <BasicColumnsGrid rows={userRows} columns={userColums} />
