cd526787c8b62b129e91e9a94a29506c7d454ff6
hritikchokker <hchokker7@gmail.com>
2021-04-02 13:26:05 +0530

    2 files modified

--- src/table/Tabletest.js
+++ src/table/Tabletest.js
@@ -3,7 +3,7 @@ import { DataGrid } from "@material-ui/data-grid";
 
 export default function BasicColumnsGrid({ rows, columns }) {
   return (
-    <div style={{ height: 250, width: "100%" }}>
+    <div style={{ height: "100vh" }}>
       <DataGrid columns={columns} rows={rows} />
     </div>
   );
--- src/user-module/userlist.js
+++ src/user-module/userlist.js
@@ -10,12 +10,39 @@ export default function UserList() {
         if (res) {
           const item = res[0];
           const clm = [];
-          Object.keys(item => {
-            clm.push({ field: item });
+          Object.keys(item).forEach(el => {
+            console.log(el, "elel");
+            if (el === "name") {
+              clm.push({
+                field: el,
+                headerName: "Name",
+                width: 160,
+                sortComparator: (v1, v2, cellParams1, cellParams2) => {
+                  // console.log(cellParams1, cellParams2, "hritik");
+                  return cellParams1.data.name.localeCompare(
+                    cellParams2.data.name
+                  );
+                }
+              });
+            } else {
+              clm.push({ field: el });
+            }
+          });
+          // {
+          //   field: 'fullName',
+          //   headerName: 'Full name',
+          //   width: 160,
+          //   valueGetter: getFullName,
+          //   sortComparator: (v1, v2, cellParams1, cellParams2) =>
+          //     getFullName(cellParams1).localeCompare(getFullName(cellParams2)),
+          // },
+          res = res.map(val => {
+            val.address = `${val.address.city} ${val.address.street}`;
+            val.company = `${val.company.name}`;
+            return val;
           });
           setUserColumns(clm);
           setUserRows(res);
-          console.log(res, "res");
         }
       })
       .catch(err => console.log(err));
