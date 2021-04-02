f3d5da3c36fd6e8e6aaf400d51fcd7ec7c43939e
hritikchokker <hchokker7@gmail.com>
2021-04-02 12:40:59 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./style.css";
-import UserList from "/user-list-module/userlist";
+import UserList from "./user-module/userlist";
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      // <BasicColumnsGrid />
       <UserList />
     </div>
   );
